f3c62b784bd8e9ba01aa095f884e4fab4f8419c9
lanccj <lanccj@163.com>
2016-11-23 16:12:48 +0800

    2016年11月23日16:12:43

---
+++ app/ITalk.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TouchableOpacity,
+    Alert
+} from 'react-native';
+
+//第三方
+import {Actions, Scene, Router} from 'react-native-router-flux';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+//定义组件
+import ErrorPage from './app/ErrorPage';
+import AppLoginPage from './app/AppLoginPage';
+import MainPage     from './app/MainPage';
+import ChatingRoom  from './app/chat/ChatingRoom';
+import RegisterPage from './app/RegisterPage';
+
+const styles = StyleSheet.create({
+    navigationBarStyle:{
+        backgroundColor:"#0584FE"
+    },
+    titleStyle:{
+        color:"#FFFFFF"
+    },
+    backTextStyle:{
+        marginLeft:5,
+        color:"#FFFFFF",
+        lineHeight:22
+    }
+});
+
+//返回按钮的定义
+const _renderBackButton=()=>{
+    return (
+        <TouchableOpacity onPress={Actions.pop}>
+            <View style={[{flexDirection:"row"}]}>
+                <Ionicons name="ios-arrow-back" size={25} color="#FFFFFF" />
+                <Text style={styles.backTextStyle}>返回</Text>
+            </View>
+        </TouchableOpacity>
+    );
+}
+
+//APP所有场景定义
+const scenes = Actions.create(
+    <Scene key="root">
+        <Scene
+            initial={true}
+            key="AppLoginPage"
+            component={AppLoginPage}
+            title="爱聊ITalk用户登录"
+            navigationBarStyle={styles.navigationBarStyle}
+            titleStyle={styles.titleStyle}
+        />
+        <Scene
+            key="RegisterPage"
+            renderBackButton={_renderBackButton}
+            component={RegisterPage}
+            title="爱聊ITalk用户注册"
+            navigationBarStyle={styles.navigationBarStyle}
+            titleStyle={styles.titleStyle}
+        />
+        <Scene
+            key="MainPage"
+            renderBackButton={_renderBackButton}
+            component={MainPage}
+            title="爱聊ITalk"
+            navigationBarStyle={styles.navigationBarStyle}
+            titleStyle={styles.titleStyle}
+        />
+        <Scene
+            key="ChatingRoom"
+            renderBackButton={_renderBackButton}
+            component={ChatingRoom}
+            title="聊天室"
+            navigationBarStyle={styles.navigationBarStyle}
+            titleStyle={styles.titleStyle}
+        />
+        <Scene key="ErrorPage" renderBackButton={_renderBackButton} component={ErrorPage}/>
+    </Scene>
+);
+
+export default class ITalk extends Component {
+    render() {
+        return (
+            <Router
+                scenes={scenes}/>
+        );
+    }
+}
+
+
+
